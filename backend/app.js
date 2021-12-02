fe99988306062a90e14b098b9636138c8b635a88
Vaishnavi Gupta <you@example.com>
2021-12-02 09:56:32 +0530

    MERN E commerce site

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const app = express()
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+const fileUpload = require('express-fileupload')
+
+
+const errorMiddleware = require('./middlewares/errors')
+
+//Setting up config file
+dotenv.config({ path: "backend/config/config.env" })
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(fileUpload())
+
+//Import all the routes
+const products = require('./routes/product')
+const auth = require('./routes/auth')
+const payment = require('./routes/payment')
+const order = require('./routes/order')
+
+app.use('/api/v1', products)
+app.use('/api/v1', auth)
+app.use('/api/v1', payment)
+app.use('/api/v1', order)
+
+//Middleware to handle errors
+app.use(errorMiddleware)
+
+module.exports = app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
